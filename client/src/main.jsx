6c56b7b1d4bcddc202da3bb26007708508d8c128
oliver23n <oliver.nasevski@gmail.com>
2023-10-18 16:13:17 -0400

    created pages files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import App from './App.jsx';
+import { Error, Login, Todo } from './pages'
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    errorElement: <Error />,
+    children:[
+      {
+        index: true,
+        element: <Todo />
+      },
+      {
+        path:'/login',
+        element: <Login />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<RouterProvider router ={router} />
 )
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,5 @@
+import Error from './Error';
+import Login from './Login';
+import Todo from './Todo';
+
+module.exports = { Error, Login, Todo};
