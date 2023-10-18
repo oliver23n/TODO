23c5cbc737314d5ab96f9f550db0534318dbaa9b
oliver23n <oliver.nasevski@gmail.com>
2023-10-18 18:47:14 -0400

    started on todos

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,4 +1,3 @@
-// import { useLocation } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container';
--- client/src/pages/Todo.jsx
+++ client/src/pages/Todo.jsx
@@ -1,9 +1,29 @@
-
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 export default function Todo() {
 
 
     return (
-        <div>this is todo </div>
+       <>
+       <Container>
+
+                <Stack direction="horizontal" gap={3}>
+                    <h3>NewTask</h3>  
+                    <Form.Control className="me-auto" placeholder="Enter new task" />
+                    <Button variant="primary">Save</Button>
+                </Stack>
+                <Container>
+                    <Row>
+                        <Col>TO be done</Col>
+                        <Col>Completed</Col>
+                    </Row>
+                </Container>
+       </Container>
+       </>
     )
 }
