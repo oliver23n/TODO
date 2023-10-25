4a27a6109a9dbe78bf134d7bd2b95c81da000973
oliver23n <oliver.nasevski@gmail.com>
2023-10-24 21:05:37 -0400

    styled some home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import Badge from 'react-bootstrap/Badge'
 import Button from 'react-bootstrap/Button'
 import Auth from '../utils/auth';
+import Container from 'react-bootstrap/esm/Container';
 
 export default function Home() {
 
     return (
-        <>
-
-            <h2>TODO! <Badge bg="secondary">!</Badge></h2>
+        <Container >
+            <div className='d-flex justify-content-center'>
+             <h1>TODO! <Badge bg="secondary">!</Badge></h1>
+            </div>
             {Auth.loggedIn() ? (<Button href='/todos'>My tasks</Button>):<p></p>
             }   
-        </>
+        </Container>
     )
 }
